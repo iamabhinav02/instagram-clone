099b455fd245d167242f057996e8fdaf5caeeea5
Abhinav Kumar <iamabhinav02@gmail.com>
2021-01-09 17:34:14 +0530

    Made project production ready

--- app.js
+++ app.js
@@ -19,4 +19,13 @@ app.use(post);
 app.use(user);
 
 const PORT = process.env.PORT || 4000;
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
 app.listen(PORT, console.log(`Server running on PORT ${PORT}...`));
